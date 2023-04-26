bb177a42f28c65092c0fb1ebbee97ffd6604a8cc
brnkgabriel <brnkgabriel@gmail.com>
2023-04-26 16:34:35 +0100

    added netlify CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,50 @@
 import { defineConfig } from 'astro/config';
 
+import NetlifyCMS from 'astro-netlify-cms';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main"
+        },
+        collections: [
+          {
+            label: "Blog",
+            name: "blog",
+            slug: "{{ title }}",
+            folder: "src/content/blog",
+            create: true,
+            delete: true,
+            fields: [
+              {
+                label: "Title",
+                name: "title",
+                widget: "string"
+              }, {
+                label: "Description",
+                name: "description",
+                widget: "string",
+              }, {
+                label: "Date",
+                name: "date",
+                widget: "datetime"
+              }, {
+                label: "Draft",
+                name: "draft",
+                widget: "boolean"
+              }, {
+                label: "Author",
+                name: "author",
+                widget: "string"
+              }
+            ]
+          }
+        ]
+      }
+    })
+  ]
+});
